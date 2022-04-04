31fd7ac4fbfa5b09239be00f9cc6bbc6b48dffc6
01FE20BEC423 <102014789+01FE20BEC423@users.noreply.github.com>
2022-04-04 23:47:41 +0530

    Update mux.v

--- mux.v
+++ mux.v
@@ -1,11 +1,6 @@
-module half_adder
-(
-  input wire a,b,
-  output wire sum,carry
-);
-
-assign sum = a ^ b;
-assign carry = a & b;
-
-endmodule
-
+module mux4to1(Y,i0,i1,i2,i3,s1,s0);
+output Y;
+input i0,i2,i1,i3;
+input s1,s0;
+assign Y= (~s1 & ~s0 & i0)|(~s1 & s0 & i1)|(s1 & ~s0 & i2)|(s1 & s0 & i3);
+endmodule 
